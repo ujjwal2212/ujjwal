2550c6a0d36dcc8041a61632c0a806b5ff5c85ef
ujjwal chakravorty <ujjwal.22chakravorty@gmail.com>
2020-10-12 20:00:13 +0530

    new array code

---
+++ Pattern/src/ArrayDemo.java
@@ -0,0 +1,20 @@
+class ArrayDemo{
+	public static void main(String args[]){
+	
+		int a[]= new int[5];
+		a[0]=70;
+		a[1]=50;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		System.out.println(a[2]);
+		for(int i=0;i<a.length;i++)
+			System.out.println(a[i]);
+		
+	}
+	
+}
+
+
+
+
